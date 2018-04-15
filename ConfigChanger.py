9823c592da2e03bce53334618109d9e185eb05e2
C3ypt1c <c3ypt1c@gmail.com>
2018-04-15 13:45:40 +0100

    Minor formattings changes

--- ConfigChanger.py
+++ ConfigChanger.py
@@ -21,16 +21,18 @@ while True:
     print ( "What would you like to change?" )
     i = 0
     for x in fileData:
-        print ( i+1, x, sep=". " ) #Mind the offset
+        if x.count("=") == 1:
+            print ( i+1, x, sep=". " ) #Mind the offset
         i += 1
 
     item = Functions.getValidIntInput(Max=i+1,Min=1)-1 #offsets cancel
     #let the user change something ^
+    varableName = string.replace(fileData[item].split("=")[0], " ", "")
 
-    print ( "What would you like to change", fileData[item].split("=")[0], "to?" )
+    print ( "What would you like to change", varableName, "to?" )
     value = input ( "> " ) #Input can by of almost any type.
     #to something ^
-    print ( "Setting ", fileData[item].split("=")[0], "to ", value, sep="" )
+    print ( "Setting", varableName, "to", value )
     fileData[item] = fileData[item].split("=")[0] + "= " + value
     #Man, this is fun
 
