0f767c08940284dba6a6d3432744dc3b18dc7a95
C3ypt1c <c3ypt1c@gmail.com>
2018-04-15 14:16:29 +0100

    Chain plan finished && format changers && large test file

--- Chain.py
+++ Chain.py
@@ -1,5 +1,6 @@
 import threading
 import itertools
+import hashlib
 import Functions
 import Config
 
@@ -10,7 +11,16 @@ difficulty = Functions.getValidIntInput(Max=5, Min=1, Default=2)
 print ( "Building file tree..." )
 files = Functions.FindFiles(Config.ApplyChainToFolder)
 
-# hack hack
+#Create the required tracker file.
+
+#Stack certain amount of data into a file.
+
+#Generate an appropriate salt so that sha512 can
+#generate a set amount of 0's at the start (will need
+#use threading to acheve best performance).
+
+#Full file structure:
+#<head>[order]<file>[filePath]lengh of data]</file></head>[DATA]<hash>sha512</hash>
 
 
 
